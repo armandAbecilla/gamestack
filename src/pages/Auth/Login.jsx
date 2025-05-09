fcf3117dcd65eb0cfdbae9ec376b27949eacb872
Armand Abecilla <dnamra.0216@gmail.com>
2025-05-10 00:00:53 +0800

    Auth forms style change

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -2,7 +2,9 @@ import LoginForm from '../../components/auth/LoginForm';
 export default function LoginPage() {
   return (
     <div className='flex h-screen items-start justify-center pt-20'>
-      <LoginForm />
+      <div className='w-full max-w-[500px]'>
+        <LoginForm />
+      </div>
     </div>
   );
 }
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -3,7 +3,9 @@ import SignupForm from '../../components/auth/SignupForm';
 export default function SignupPage() {
   return (
     <div className='flex h-screen items-start justify-center pt-20'>
-      <SignupForm />
+      <div className='w-full max-w-[500px]'>
+        <SignupForm />
+      </div>
     </div>
   );
 }
