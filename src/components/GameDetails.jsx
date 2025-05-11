1cd457fb0ea709ee68cd31ce29ef3d3540ca0fb4
Armand Abecilla <dnamra.0216@gmail.com>
2025-05-11 16:47:54 +0800

    Added functionality to add or remove a game from library

---
+++ src/components/GameDetails.jsx
@@ -0,0 +1,111 @@
+import GameDetailsSkeleton from './skeleton-loaders/GameDetails';
+import Button from './UI/Button';
+
+export default function GameDetails({
+  gameData,
+  isLoading,
+  onAddOrRemoveToList,
+  userGameData,
+}) {
+  const platforms = gameData
+    ? gameData.platforms.map((item, index) => (
+        <span key={item.platform.id}>
+          {item.platform.name}
+          {index !== gameData.platforms.length - 1 ? ', ' : ''}
+        </span>
+      ))
+    : [];
+
+  const developers = gameData
+    ? gameData.developers.map(
+        (devs, index) =>
+          `${devs.name}${index !== gameData.developers.length - 1 ? ', ' : ''}`,
+      )
+    : [];
+
+  const pcPlatform = gameData
+    ? gameData.platforms.find((item) => item.platform.name === 'PC')
+    : null;
+
+  // System Requirements
+  const minimumRequirement = pcPlatform ? (
+    <span className='mb-1 block whitespace-pre-line'>
+      {pcPlatform?.requirements?.minimum ||
+        'Minimum requirements not yet available'}
+    </span>
+  ) : undefined;
+  const recommendedRequirement = pcPlatform ? (
+    <span className='mb-1 block whitespace-pre-line'>
+      {pcPlatform?.requirements?.recommended ||
+        'Recommended requirements not yet available'}
+    </span>
+  ) : undefined;
+
+  //  skeleton loader
+  if (isLoading) {
+    return <GameDetailsSkeleton />;
+  }
+
+  const onUserList = userGameData && !!userGameData;
+
+  console.log(userGameData);
+
+  return (
+    <>
+      {gameData && !isLoading && (
+        <div>
+          <div className='flex gap-5'>
+            <img
+              className='aspect-auto h-[700px] max-w-[500px] object-cover'
+              src={gameData.background_image}
+            />
+            <div className='flex flex-col gap-4'>
+              <h1 className='font-heading text-5xl'>{gameData.name}</h1>
+              <h4 className='text-stone-100'>
+                Release date: {gameData.released}
+              </h4>
+              <div className='flex gap-2'>
+                <h4 className='text-stone-100'>Available on:</h4>
+                <p>{platforms}</p>
+              </div>
+              <span className='text-stone-100'>
+                Metacritic Score: {gameData.metacritic || 'Not yet available'}
+              </span>
+              <span className='text-stone-100'>Developed by: {developers}</span>
+              <h4 className='font-h mt-5 text-xl'>Description:</h4>
+              <div
+                className='flex flex-col gap-2 text-stone-300'
+                dangerouslySetInnerHTML={{
+                  __html: gameData.description,
+                }}
+              ></div>
+
+              {/* user options */}
+              <div className='mt-4'>
+                <Button onClick={onAddOrRemoveToList}>
+                  {!onUserList ? 'Add to Library' : 'Remove to Library'}
+                </Button>
+              </div>
+
+              {/* <div>
+                   <h5 className='text-lg text-stone-300'>Notes:</h5>
+                   <p className='text-stone-300'>
+                     Add this game to your list to start adding notes!
+                   </p>
+                 </div> */}
+            </div>
+          </div>
+
+          <div className='mt-4 border-t border-stone-700 pt-4'>
+            <h4 className='text-xl text-stone-200'>PC System Requirements</h4>
+
+            <div className='flex gap-15'>
+              <div className='mt-4 basis-1/2'>{minimumRequirement}</div>
+              <div className='mt-4 basis-1/2'>{recommendedRequirement}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- src/pages/Game/GameDetail.jsx
+++ src/pages/Game/GameDetail.jsx
@@ -5,112 +5,86 @@ import axios from 'axios';
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-import GameDetailsSkeleton from '../../components/skeleton-loaders/GameDetails';
+import GameDetails from '../../components/GameDetails';
+import { useSelector } from 'react-redux';
 
 const fetchGameDetails = async (gameId) => {
   const result = await axios.get(`${projectConfig.API_URL}/rawg/${gameId}`);
   return result.data;
 };
 
+const fetchUserGameDetail = async (gameId, userId) => {
+  const result = await axios.get(
+    `${projectConfig.API_URL}/games/${gameId}?userId=${userId}`,
+  );
+  return result.data[0];
+};
+
+const addGameToList = async (data) => {
+  const result = await axios.post(`${projectConfig.API_URL}/games/add`, data);
+  return result.data;
+};
+
+const removeGameFromList = async (id) => {
+  await axios.delete(`${projectConfig.API_URL}/games/${id}`);
+};
+
 export default function GameDetailPage() {
+  const auth = useSelector((state) => state.auth);
+  const params = useParams();
+  // game information
   const [gameData, setGameData] = useState();
   const [isGameFetching, setIsGameFetching] = useState(false);
-  const params = useParams();
+  // user information for the selected game
+  const [userGameData, setUserGameData] = useState();
 
   useEffect(() => {
     const fetchSelectedGame = async () => {
       setIsGameFetching(true);
-      const gameDetails = await fetchGameDetails(params.id);
-      setGameData(gameDetails);
+
+      const [fetchedGameDetails, fetchedUserGameData] = await Promise.all([
+        fetchGameDetails(params.id),
+        fetchUserGameDetail(params.id, auth.user.id),
+      ]);
+
+      setGameData(fetchedGameDetails);
+      setUserGameData(fetchedUserGameData);
       setIsGameFetching(false);
     };
 
     fetchSelectedGame();
-  }, [params.id]);
+  }, [params.id, auth.user.id]);
 
-  //  skeleton loader
-  if (isGameFetching) {
-    return <GameDetailsSkeleton />;
+  async function handleAddRemoveToList() {
+    // if action == add to library
+    if (!userGameData) {
+      const data = {
+        userId: auth.user.id,
+        rawgGameId: params.id,
+        status: 'whishlist',
+        notes: 'test',
+      };
+
+      const res = await addGameToList(data);
+      const newUserGameData = res.data[0];
+
+      setUserGameData(newUserGameData);
+    }
+
+    // if action == remove from library
+    if (userGameData) {
+      removeGameFromList(userGameData.id);
+      setUserGameData(null);
+    }
   }
 
-  const platforms = gameData
-    ? gameData.platforms.map((item, index) => (
-        <span key={item.platform.id}>
-          {item.platform.name}
-          {index !== gameData.platforms.length - 1 ? ', ' : ''}
-        </span>
-      ))
-    : [];
-
-  const developers = gameData
-    ? gameData.developers.map(
-        (devs, index) =>
-          `${devs.name}${index !== gameData.developers.length - 1 ? ', ' : ''}`,
-      )
-    : [];
-
-  const pcPlatform = gameData
-    ? gameData.platforms.find((item) => item.platform.name === 'PC')
-    : null;
-
-  // System Requirements
-  const minimumRequirement = pcPlatform ? (
-    <span className='mb-1 block whitespace-pre-line'>
-      {pcPlatform?.requirements?.minimum ||
-        'Minimum requirements not yet available'}
-    </span>
-  ) : undefined;
-  const recommendedRequirement = pcPlatform ? (
-    <span className='mb-1 block whitespace-pre-line'>
-      {pcPlatform?.requirements?.recommended ||
-        'Recommended requirements not yet available'}
-    </span>
-  ) : undefined;
-
   return (
-    <>
-      {gameData && !isGameFetching && (
-        <div>
-          <div className='flex gap-5'>
-            <img
-              className='aspect-auto h-[700px] max-w-[500px] object-cover'
-              src={gameData.background_image}
-            />
-            <div className='flex flex-col gap-4'>
-              <h1 className='font-heading text-5xl'>{gameData.name}</h1>
-              <h4 className='text-stone-100'>
-                Release date: {gameData.released}
-              </h4>
-              <div className='flex gap-2'>
-                <h4 className='text-stone-100'>Available on:</h4>
-                <p>{platforms}</p>
-              </div>
-              <span className='text-stone-100'>
-                Metacritic Score: {gameData.metacritic || 'Not yet available'}
-              </span>
-              <span className='text-stone-100'>Developed by: {developers}</span>
-              <h4 className='font-h mt-5 text-xl'>Description:</h4>
-              <div
-                className='flex flex-col gap-2 text-stone-300'
-                dangerouslySetInnerHTML={{
-                  __html: gameData.description,
-                }}
-              ></div>
-            </div>
-          </div>
-
-          <div className='mt-4 border-t border-stone-700 pt-4'>
-            <h4 className='text-xl text-stone-200'>PC System Requirements</h4>
-
-            <div className='flex gap-15'>
-              <div className='mt-4 basis-1/2'>{minimumRequirement}</div>
-              <div className='mt-4 basis-1/2'>{recommendedRequirement}</div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
+    <GameDetails
+      gameData={gameData}
+      isLoading={isGameFetching}
+      onAddOrRemoveToList={handleAddRemoveToList}
+      userGameData={userGameData}
+    />
   );
 }
 
